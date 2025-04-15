0027edd704dc8f3f6cad984d5ab70dad91911733
KaremMetrial <karim.metrial@gmail.com>
2025-04-15 22:29:45 +0200

    Add Category API with resource handling and error responses

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
     protected $fillable = ['name', 'slug'];
 
-
-    public function images()
+    public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return $this->image ? Storage::url($this->image->url) : null;
     }
 }
